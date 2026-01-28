1afd0ef394aa2b18b91b625a9e098dede2179185
rajatbisht01 <rajat.bisht@solutionbriz.com>
2026-01-28 17:39:11 +0530

    update_google_search_n_redirect_issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { lazy, Suspense } from "react";
 // import { servicesConfig } from "@/config/serviceConfig";
 import ScrollToTop from "./components/ScrollToTop";
@@ -136,6 +136,7 @@ const App = () => (
         <ScrollToTop />
         <Suspense fallback={<PageLoader />}>
           <Routes>
+          <Route path="/home" element={<Navigate to="/" replace />} />
             {/* Main Pages */}
             <Route path="/" element={<Index />} />
             <Route path="/about" element={<About />} />
